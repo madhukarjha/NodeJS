102aa495f0a398ad565701c123370c751001cfa3
Madhukar Jha <madhukar.jha@gmail.com>
2021-09-23 18:55:07 +0530

    hashing code

---
+++ 0hash.js
@@ -0,0 +1,21 @@
+process.env.UV_THREADPOOL_SIZE = 4;
+
+const crypto = require('crypto');
+
+var start =Date.now();
+ 
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+    console.log('1: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+    console.log('2: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+    console.log('3: ', Date.now() - start);
+});
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+    console.log('4: ', Date.now() - start);
+});
+ 
